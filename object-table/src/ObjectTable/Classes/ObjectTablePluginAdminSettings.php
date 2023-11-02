4dd6ce9336c9d24315569f495d979dce2580e51d
Barry Brands <barrybrands02@hotmail.com>
2023-11-02 17:45:59 +0100

    Changed settings page, added mapping and changed table

--- object-table/src/ObjectTable/Classes/ObjectTablePluginAdminSettings.php
+++ object-table/src/ObjectTable/Classes/ObjectTablePluginAdminSettings.php
@@ -11,34 +11,114 @@ class ObjectTablePluginAdminSettings
 
         // Initiating the settings page
         add_action('admin_init', [$this, 'wporg_settings_init']);
+        add_action('admin_post_objecttable_add_config', [$this, 'handle_add_config']);
+
+        // Handle removing a config
+        add_action('admin_post_objecttable_remove_config', [$this, 'handle_remove_config']);
+
+        // Use css
+        add_action('admin_enqueue_scripts', [$this, 'enqueue_admin_styles']);
     }
 
-    /**
-     * Lets define the basic settings page
-     */
+    public function handle_remove_config()
+    {
+        // Check user
+        if (!current_user_can('manage_options')) {
+            wp_die('Unauthorized user');
+        }
+
+        // Get existing configs
+        $configs = get_option('objecttable_configs', []);
+
+        // Get the ID of the config to remove
+        $remove_id = intval($_POST['remove_config_id']);
+
+        // Remove the config with the specified ID
+        $configs = array_filter($configs, function($config) use ($remove_id) {
+            return $config['id'] !== $remove_id;
+        });
+
+        // Update the option in the database
+        update_option('objecttable_configs', array_values($configs));
+
+        // Redirect back to the settings page
+        wp_redirect(admin_url('options-general.php?page=objecttable'));
+        exit;
+    }
+
+    public function enqueue_admin_styles()
+    {
+        wp_enqueue_style(
+            'objecttable-admin-styles',
+            plugin_dir_url(dirname(__FILE__, 3)) . 'src/ObjectTable/assets/css/admin-styles.css',
+            array(),
+            filemtime(plugin_dir_path(dirname(__FILE__, 3)) . 'src/ObjectTable/assets/css/admin-styles.css') 
+        );
+    }
+
+    public function handle_add_config()
+    {
+        // Check user
+        if (!current_user_can('manage_options')) {
+            wp_die('Unauthorized user');
+        }
+        
+        // Get existing configs
+        $configs = get_option('objecttable_configs', []);
+        
+        // Get the last used ID, or default to 0 if it doesn't exist
+        $last_id = get_option('objecttable_last_id', 0);
+        
+        // Increment the ID for the new config
+        $new_id = $last_id + 1;
+        
+        // Prepare new config data
+        $new_config = [
+            'id' => $new_id,
+            'url' => sanitize_text_field($_POST['new_config_url']),
+            'key' => sanitize_text_field($_POST['new_config_key']),
+            'mapping' => isset($_POST['new_config_mapping']) ? stripslashes($_POST['new_config_mapping']) : null 
+        ];
+        
+        // Add new config to the existing configs array
+        $configs[] = $new_config;
+        
+        // Update the option in the database
+        update_option('objecttable_configs', $configs);
+        
+        // Update the last used ID in the database
+        update_option('objecttable_last_id', $new_id);
+        
+        // Redirect back to the settings page
+        wp_redirect(admin_url('options-general.php?page=objecttable'));
+        exit;
+    }
+    
+    
     public function objecttable_options_page_html()
     {
-        // check user capabilities
+        // Check user
         if (!current_user_can('manage_options')) {
             return;
         }
+        
+        $configs = get_option('objecttable_configs', []);
 
-?>
-        <div class="wrap">
-            <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
-            <form action="options.php" method="post">
-                <?php
-                // output security fields for the registered setting "wporg_options"
-                settings_fields('objecttable_options');
-                // output setting sections and their fields
-                // (sections are registered for "wporg", each field is registered to a specific section)
-                do_settings_sections('objecttable_api');
-                // output save settings button
-                submit_button(__('Save Settings', 'textdomain'));
-                ?>
-            </form>
-        </div>
-    <?php
+        // Start wrap.
+        echo '<div class="wrap">';
+
+        // Title.
+        echo '<h1 class="wp-heading-inline">ObjectTable - Manage API configurations</h1>';
+
+        // Description.
+        echo '<h2>How to use shortcodes</h2>';
+        echo '<p>A shortcode can be added to a page like [object-table configId="1"] where configId is a id of a configuration chosen from the table below you want to fetch and render.</p>';
+        
+        $this->renderForm();
+        $this->renderTable($configs);
+
+        // Close wrap.
+        echo '</div>';
     }
 
     /**
@@ -61,34 +141,12 @@ class ObjectTablePluginAdminSettings
      */
     public function wporg_settings_init()
     {
-        // register a new setting for "reading" page
-        register_setting('objecttable_options', 'objecttable_api_domain');
-        register_setting('objecttable_options', 'objecttable_api_key');
-
         // register a new section in the "reading" page
         add_settings_section(
             'default', // id
             'API  Configuration', // title
             [$this, 'wporg_settings_section_callback'], // callback
             'objecttable_api' // page
-        );
-
-        // register a new field in the "wporg_settings_section" section, inside the "reading" page
-        add_settings_field(
-            'objecttable_api_domain_field', // id
-            'API Domain',  // title
-            [$this, 'objecttable_api_domain_field_callback'], //callback
-            'objecttable_api',
-            'default'
-        );
-
-        // register a new field in the "wporg_settings_section" section, inside the "reading" page
-        add_settings_field(
-            'objecttable_api_key_field',
-            'API  KEY',
-            [$this, 'objecttable_api_key_field_callback'],
-            'objecttable_api',
-            'default'
         );
     }
 
@@ -102,24 +160,61 @@ class ObjectTablePluginAdminSettings
         echo '<p>In order to use the objecttable api you wil need to provide api credentials.</p>';
     }
 
-    // field content cb
-    public function objecttable_api_domain_field_callback()
+    private function renderForm()
     {
-        // get the value of the setting we've registered with register_setting()
-        $setting = get_option('objecttable_api_domain');
-        // output the field
-    ?>
-        <input type="text" name="objecttable_api_domain" value="<?php echo isset($setting) ? esc_attr($setting) : ''; ?>">
-    <?php
+        // Form for adding new config
+        echo '<h2>Add a configuration</h2>';
+        echo '<form method="post" action="' . admin_url('admin-post.php') . '" class="objecttable-form">';  
+        echo '<div class="objecttable-form-field-row">';  
+        echo '<div class="objecttable-form-field">';
+        echo '<input type="hidden" name="action" value="objecttable_add_config">'; 
+        echo '<input type="text" name="new_config_url" placeholder="API URL" class="objecttable-input" required>';
+        echo '</div>';
+        echo '<div class="objecttable-form-field">';
+        echo '<input type="text" name="new_config_key" placeholder="API Key" class="objecttable-input" required>';
+        echo '</div>';
+        echo '</div>';  
+        echo '<div class="objecttable-form-field objecttable-form-field-full">'; 
+        echo '<textarea name="new_config_mapping" placeholder="Mapping (JSON)" class="objecttable-textarea"></textarea>';
+        echo '</div>';
+        echo '<div class="objecttable-form-submit">';
+        echo '<input type="submit" value="Add Config" class="objecttable-submit">';
+        echo '</div>';
+        echo '</form>';
     }
+    
 
-    public function objecttable_api_key_field_callback()
+    private function renderTable(array $configs)
     {
-        // get the value of the setting we've registered with register_setting()
-        $setting = get_option('objecttable_api_key');
-        // output the field
-    ?>
-        <input type="text" name="objecttable_api_key" value="<?php echo isset($setting) ? esc_attr($setting) : ''; ?>">
-    <?php
+        // Table for viewing existing configs  
+        echo '<h2>Existing configurations</h2>';
+        echo '<table class="objecttable-table">';
+        echo '<thead>';
+        echo '<tr>';
+        echo '<th>ID</th>';  
+        echo '<th>API URL</th>';  
+        echo '<th>API Key</th>';  
+        echo '<th>Mapping</th>';  // New column header
+        echo '<th></th>';  
+        echo '</tr>';
+        echo '</thead>';
+        echo '<tbody>';
+        foreach($configs as $config) {
+            echo '<tr>';
+            echo '<td>' . esc_html($config['id']) . '</td>';
+            echo '<td>' . esc_html($config['url']) . '</td>';
+            echo '<td>' . esc_html($config['key']) . '</td>';
+            echo '<td><pre>' . (isset($config['mapping']) ? esc_html($config['mapping']) : '') . '</pre></td>';  // New column data
+            echo '<td>';
+            echo '<form method="post" action="' . admin_url('admin-post.php') . '">';
+            echo '<input type="hidden" name="action" value="objecttable_remove_config">';
+            echo '<input type="hidden" name="remove_config_id" value="' . esc_attr($config['id']) . '">';
+            echo '<input type="submit" value="Remove">';
+            echo '</form>';
+            echo '</td>';
+            echo '</tr>';
+        }
+        echo '</tbody>';
+        echo '</table>';
     }
 }
--- object-table/src/ObjectTable/Classes/ObjectTablePluginShortcodes.php
+++ object-table/src/ObjectTable/Classes/ObjectTablePluginShortcodes.php
@@ -13,6 +13,19 @@ class ObjectTablePluginShortcodes
     {
         $this->plugin = $plugin;
         $this->add_shortcode();
+
+        // Use css
+        add_action('wp_enqueue_scripts', [$this, 'enqueue_styles']);
+    }
+
+    public function enqueue_styles()
+    {
+        wp_enqueue_style(
+            'objecttable-styles',
+            plugin_dir_url(dirname(__FILE__, 3)) . 'src/ObjectTable/assets/css/style.css',
+            array(),
+            filemtime(plugin_dir_path(dirname(__FILE__, 3)) . 'src/ObjectTable/assets/css/style.css') 
+        );
     }
 
     private function add_shortcode(): void
@@ -27,38 +40,67 @@ class ObjectTablePluginShortcodes
      */
     public function objecttable_result_shortcode($atts): string
     {
-        $endpoint = $atts['endpoint'] ?? '';
+        $configId = $atts['configid'] ?? '';
 
-        $key      = get_option('objecttable_api_key', '');
-        $domain   = get_option('objecttable_api_domain', '');
-
-        if (empty($key) || empty($domain)|| empty($endpoint)) {
-            return '';
+        if (empty($configId) === true) {
+            return '<p>No configId given to shortcode.</p>';
         }
 
-        $url = $domain.$endpoint.'?_limit=1000';
+        $correctConfig = null;
+        $configs = get_option('objecttable_configs', []);
+        foreach ($configs as $config) {
+            if ($config['id'] == $configId) { 
+                $correctConfig = $config;
+                break;
+            }
+        }
+
+        if (isset($correctConfig) === false) {
+            return '<p>Could not find a configuration with given id.</p>';
+        }
+
+        $config = $correctConfig;        
+
+        if (isset($config['url']) === false || isset($config['key']) === false) {
+            return '<p>Configuration has no url or api key.</p>';
+        }
+
+        $decodedBody = $this->fetchData($config['url'],  $config['key']);
+        // Return error.
+        if (is_string($decodedBody) === true) {
+            return $decodedBody;
+        }
+
+        $mapping = isset($config['mapping']) ? json_decode($config['mapping'], true) : null;
+
+        return $this->shortcodeResult($decodedBody['results'], $mapping);
+    }
+
+    private function fetchData(string $url, string $apiKey)
+    {
+        $url .= '?_limit=1000';
 
         $data = wp_remote_get($url, [
-            'headers'     => ['Content-Type' => 'application/json;', 'Authorization' => $key]
+            'headers'     => ['Content-Type' => 'application/json;', 'Authorization' => $apiKey]
         ]);
 
         if (is_wp_error($data)) {
-            return '';
+            return '<p>Error retrieving data.</p>';
         }
 
         $responseBody = wp_remote_retrieve_body($data);
 
         if (is_wp_error($responseBody)) {
-            return '';
+            return '<p>Error retrieving data.</p>';
         }
 
         $decodedBody = json_decode($responseBody, true);
 
         if (array_key_exists('results', $decodedBody) === false) {
-            return '';
+            return '<p>Error decoding data.</p>';
         }
 
-        return $this->shortcodeResult($decodedBody['results']);
+        return $decodedBody;
     }
 
     /**
@@ -68,35 +110,78 @@ class ObjectTablePluginShortcodes
      *
      * @return string
      */
-    private function shortcodeResult(?array $objects = []): string
+    private function shortcodeResult(?array $objects = [], ?array $mapping = null): string
     {
         if (empty($objects) || isset($objects[0]) === false) {
             return '<div>' . esc_html__('Er ging iets fout met het ophalen van data.', 'objecttableaddon') . '</div>';
         }
-    
+
+        $filteredHeaders = [];
+        $tableHeaderRow  = $this->createTableHeader($objects, $mapping, $filteredHeaders);
+
+        $tableBodyRows   = $this->createTableRows($objects, $filteredHeaders);
+
+        $tableHtml = '<div class="table-container"><table>' . $tableHeaderRow . $tableBodyRows . '</table></div>';
+
+        return '<div>' . $tableHtml . '</div>';
+    }
+
+    private function createTableHeader(?array &$objects = [], ?array $mapping = null, array &$filteredHeaders): string
+    {
+        // Initial filtering of headers
         $headers = array_keys($objects[0]);
         $filteredHeaders = array_filter($headers, function($key) {
             return strpos($key, '_') !== 0 && $key !== 'id';
         });
-    
+
+        if ($mapping) {
+            // If a mapping is provided, modify objects to only include mapped keys
+            $objects = array_map(function($object) use ($mapping) {
+                $newObject = [];
+                foreach ($mapping as $newKey => $map) {
+                    $keys = explode('.', $map);
+                    $value = $object;
+                    foreach ($keys as $key) {
+                        if (!isset($value[$key])) {
+                            $value = '';  // Set value to an empty string if key is not found
+                            break;  // Break out of the inner loop
+                        }
+                        $value = $value[$key];
+                    }
+                    $newObject[$newKey] = $value;
+                }
+                return $newObject;
+            }, $objects);
+            // Update filteredHeaders to reflect the keys of the mapped objects
+            $filteredHeaders = array_keys($objects[0]);
+        }
+
         $tableHeaderRow = '<tr>';
         foreach ($filteredHeaders as $header) {
             $tableHeaderRow .= "<th>{$header}</th>";
         }
         $tableHeaderRow .= '</tr>';
-    
+
+        return $tableHeaderRow;
+    }
+
+    private function createTableRows(?array $objects = [], array $filteredHeaders): string
+    {
         $tableBodyRows = '';
         foreach ($objects as $object) {
             $tableBodyRow = '<tr>';
             foreach ($filteredHeaders as $header) {
-                $tableBodyRow .= "<td>{$object[$header]}</td>";
+                $value = isset($object[$header]) ? $object[$header] : '';  // Check if key exists
+                if (is_array($value)) {
+                    $value = json_encode($value);
+                }
+                $tableBodyRow .= "<td>{$value}</td>";
             }
             $tableBodyRow .= '</tr>';
             $tableBodyRows .= $tableBodyRow;
         }
     
-        $tableHtml = '<table>' . $tableHeaderRow . $tableBodyRows . '</table>';
-        
-        return '<div>' . $tableHtml . '</div>';
+        return $tableBodyRows;
     }
+    
 }
